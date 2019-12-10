eb8c231d08b433c91c70d3e8c6ff9071201d6ef9
KEINOS <github@keinos.com>
2019-12-10 10:59:26 +0900

    Update main.go

--- src/main.go
+++ src/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
     "net/http"
-
+    "encoding/json"
     "github.com/labstack/echo"
 )
 
+type M map[string]interface{}
+
 func main() {
+
+    response := M{
+        "success": true,
+        "message": "Request received successfully",
+        "data": M{
+            "responce" : "Hello, world!",
+        },
+    }
+
+    bytes, _ := json.MarshalIndent(response, "", "    ")
+
     e := echo.New()
     e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
+        return c.String(http.StatusOK, string(bytes))
     })
     e.Logger.Fatal(e.Start(":8080"))
 }
